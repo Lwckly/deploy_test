a4020511d47280435bfaaf8bcc396add8e8b9df5
Eric Efthimios <128000417+Lwckly@users.noreply.github.com>
2025-11-06 09:57:18 -0300

    Add files via upload

---
+++ test/app.py
@@ -0,0 +1,18 @@
+import openai
+from flask import Flask, render_template, request, jsonify
+import tempfile, os
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST","GET"])
+def index():
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
+
